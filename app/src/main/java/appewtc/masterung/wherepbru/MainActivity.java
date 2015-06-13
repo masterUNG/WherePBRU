5d0987945cc33222df2a8f2560268c7bdc7ae18d
masterUNG <phrombutr@gmail.com>
2015-06-13 13:52:45 +0700

    Get Location

--- app/src/main/java/appewtc/masterung/wherepbru/MainActivity.java
+++ app/src/main/java/appewtc/masterung/wherepbru/MainActivity.java
@@ -1,19 +1,164 @@
 package appewtc.masterung.wherepbru;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private int mapAnInt;
+    private TextView latTextView, lngTextView;
+    private LocationManager objLocationManager;
+    private Criteria objCriteria;
+    private boolean gpsABoolean, networkABoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Get Location
+        getLocation();
+
+
     }   // onCreate
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        gpsABoolean = objLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsABoolean) {
+            networkABoolean = objLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!networkABoolean) {
+                Intent objIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(objIntent);
+            }
+        }
+
+    }   //onStart
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setupAll();
+
+    }   // onResume
+
+    private void setupAll() {
+
+        objLocationManager.removeUpdates(objLocationListener);
+
+        String strLat = "Unknow";
+        String strLng = "Unknow";
+
+        Location networkLocation = requestUpdateFromProvider(LocationManager.NETWORK_PROVIDER, "Cannot Connected Internet");
+        if (networkLocation != null) {
+            strLat = String.format("%.7f", networkLocation.getLatitude());
+            strLng = String.format("%.7f", networkLocation.getLongitude());
+        }
+
+
+        Location gpsLocation = requestUpdateFromProvider(LocationManager.GPS_PROVIDER, "GPS error");
+        if (gpsLocation != null) {
+            strLat = String.format("%.7f", gpsLocation.getLatitude());
+            strLng = String.format("%.7f", gpsLocation.getLongitude());
+        }
+
+
+
+        latTextView.setText(strLat);
+        lngTextView.setText(strLng);
+
+    }   //setupAll
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        objLocationManager.removeUpdates(objLocationListener);
+
+    }   // onStop
+
+    //Create Event Change Time/ Change Distance
+    public Location requestUpdateFromProvider(final String strProvider, String strError) {
+
+        Location objLocation = null;
+
+        if (objLocationManager.isProviderEnabled(strProvider)) {
+
+            objLocationManager.requestLocationUpdates(strProvider, 1000, 10, objLocationListener);
+            objLocation = objLocationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Toast.makeText(MainActivity.this, strError, Toast.LENGTH_SHORT).show();
+        }
+
+        return objLocation;
+    }
+
+
+    //Create Listener
+    public final LocationListener objLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latTextView.setText(String.format("%.7f", location.getLatitude()));
+            lngTextView.setText(String.format("%.7f", location.getLongitude()));
+
+        }   //onLocationChanged
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+
+
+    private void getLocation() {
+
+        objLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        objCriteria = new Criteria();
+        objCriteria.setAccuracy(Criteria.ACCURACY_FINE);
+        objCriteria.setAltitudeRequired(false);
+        objCriteria.setBearingRequired(false);
+
+    }   //getLocation
+
+    private void bindWidget() {
+        latTextView = (TextView) findViewById(R.id.txtLat);
+        lngTextView = (TextView) findViewById(R.id.txtLng);
+    }
 
     public void clickArchitech(View view) {
         myIntent(13.06967551, 99.9786973);
@@ -39,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
         Intent objIntent = new Intent(MainActivity.this, MapsActivity.class);
         objIntent.putExtra("lat", douLat);
         objIntent.putExtra("lng", douLng);
+        objIntent.putExtra("mapType", mapAnInt);
         startActivity(objIntent);
     }
 
@@ -51,14 +197,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.itemNormal:
+                mapAnInt = 1;
+                break;
+            case R.id.itemSatellite:
+                mapAnInt = 2;
+                break;
+            case R.id.itemTerrain:
+                mapAnInt = 3;
+                break;
+            case R.id.itemHybrid:
+                mapAnInt = 4;
+                break;
+            default:
+                mapAnInt = 1;
+                break;
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/appewtc/masterung/wherepbru/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/wherepbru/MapsActivity.java
@@ -6,13 +6,17 @@ import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private double latCenterADouble, lngCenterADouble;
-    private LatLng centerLatLng;
+    private LatLng centerLatLng, engineerLatLng, computerLatLng,
+                    accountLatLng, humentLatLng, architectLatLng,
+                    cardLatLng, section1LatLng, section2LatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,16 @@ public class MapsActivity extends FragmentActivity {
         latCenterADouble = getIntent().getExtras().getDouble("lat");
         lngCenterADouble = getIntent().getExtras().getDouble("lng");
         centerLatLng = new LatLng(latCenterADouble, lngCenterADouble);
+
+        //Setup for Maker
+        engineerLatLng = new LatLng(13.07019806, 99.97874022);
+        computerLatLng = new LatLng(13.07263311, 99.97739911);
+        accountLatLng = new LatLng(13.07315564, 99.97946978);
+        humentLatLng = new LatLng(13.07220462, 99.98032808);
+        architectLatLng = new LatLng(13.06967551, 99.9786973);
+        cardLatLng = new LatLng(13.06927838, 99.97543573);
+        section1LatLng = new LatLng(13.070362, 99.976589);
+        section2LatLng = new LatLng(13.070691, 99.978491);
 
     }   //createLatLng
 
@@ -59,6 +73,81 @@ public class MapsActivity extends FragmentActivity {
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 17));
 
+        //Create MapType
+        createMapType();
+
+        //Create Maker
+        createMaker();
+
     }   // setupMap
+
+    private void createMapType() {
+
+        //Receive mapType from MainActivity
+        int intMap = getIntent().getExtras().getInt("mapType");
+
+        switch (intMap) {
+            case 1:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case 2:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case 3:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case 4:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            default:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+        }
+
+    }   //createMapType
+
+    private void createMaker() {
+
+        //From my Image
+        mMap.addMarker(new MarkerOptions()
+                .position(engineerLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build1))
+                .title("วิศวะ")
+                .snippet("คือที่ ที่เราเรียนวิศวะ กัน"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(computerLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build2))
+                .title("คณะคอมพิวเตอร์")
+                .snippet("ศูนย์คอมพิวเตอร์"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(accountLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build3))
+                .title("บัญชี")
+                .snippet("ที่ที่เราเรียนบัญชีกัน"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(humentLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build4)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(architectLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build5)));
+
+        //From default
+        mMap.addMarker(new MarkerOptions().position(cardLatLng));
+
+        //Change Color
+        mMap.addMarker(new MarkerOptions()
+                        .position(section1LatLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        mMap.addMarker(new MarkerOptions()
+                        .position(section2LatLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+
+    }   // createMaker
 
 }   // Main Class
